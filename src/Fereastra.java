a1cadf4af0908b25da4f2c8cdc077c1b1410649e
sergiu tica <mykymoto24@gmail.com>
2021-12-23 21:29:05 +0200

    Modificat modul in care se vor afisa panel-urile

--- src/Fereastra.java
+++ src/Fereastra.java
@@ -1,58 +1,137 @@
 import javax.swing.*;
-import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
 
-public class Fereastra implements ActionListener {
-	private JFrame frame;
-	private JPanel panelIntraCont;
-	private JButton butonIntraCont;
+public class Fereastra extends JFrame {
+	private JLabel lSerieCI, lNrCI, lNrTel;
+	private JTextField tSerieCI, tNrCI, tNrTel;
+	private JButton bIntraCont, bExit;
+	private JPanel pLogIn, pAlegereFilme, pChitanta;
+	private AscultatoriButoane ab;
 
-	Fereastra() {
-		frame = new JFrame("Inchiriere filme");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setMaximumSize(new Dimension(200, 200));
+	Fereastra(String titlu) {
+		super(titlu);
+		ab = new AscultatoriButoane();
 
 		initPanelLogIn();
 
-		frame.pack();
-		frame.setVisible(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
 	}
 
 	private void initPanelLogIn() {
-		FlowLayout layout = new FlowLayout();
-		layout.setAlignment(FlowLayout.CENTER);
-		panelIntraCont = new JPanel(layout);
+		lSerieCI = new JLabel("Serie CI:");
+		lNrCI = new JLabel("Numar CI:");
+		lNrTel = new JLabel("Numar telefon:");
 
-		MaskFormatter formatter;
-		JFormattedTextField serieCI = null;
-		JLabel labelSerieCI;
-		try {
-			formatter = new MaskFormatter("??");
-			labelSerieCI = new JLabel("Seria CI:");
-			labelSerieCI.setLabelFor(serieCI);
-			serieCI = new JFormattedTextField(formatter);
-			serieCI.setText("");
-			panelIntraCont.add(labelSerieCI);
-			panelIntraCont.add(serieCI);
-		} catch (ParseException e) {
-			System.out.println(e.getMessage());
-		}
+		tSerieCI = new JTextField(10);
+		tNrCI = new JTextField(10);
+		tNrTel = new JTextField(10);
 
-		butonIntraCont = new JButton("apasa-ma");
-		butonIntraCont.addActionListener(this);
-		panelIntraCont.add(butonIntraCont);
+		bExit = new JButton("Exit");
+		bIntraCont = new JButton("Intrati in cont");
+		bExit.addActionListener(ab);
+		bIntraCont.addActionListener(ab);
 
-		frame.add(panelIntraCont);
+		pLogIn = new JPanel(new GridLayout(4, 2, 5, 5));
+		pLogIn.add(lSerieCI);
+		pLogIn.add(tSerieCI);
+		pLogIn.add(lNrCI);
+		pLogIn.add(tNrCI);
+		pLogIn.add(lNrTel);
+		pLogIn.add(tNrTel);
+		pLogIn.add(bExit);
+		pLogIn.add(bIntraCont);
+
+		getContentPane().add(pLogIn);
+
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == butonIntraCont) {
-			panelIntraCont.setVisible(false);
+	private void initPanelAlegereFilme() {
+		JLabel lText = new JLabel("Alegeti maxim 5 filme pe care doriti sa le inchiriati\n");
+
+		String[] header = {"Nr. crt.", "Nume", "Gen", "An aparitie", "Ales"};
+		String[][] data = {
+				{"1.", "Dune", "Actiune", "2021", "Da"},
+				{"2.", "Luca", "Animatie", "2020", "Da"},
+				{"3.", "Spider-man", "Aventura", "2021", "Nu"},
+				{"0.", "Film 1", "Actiune", "2020", "Ales"},
+				{"1.", "Film 2", "Actiune", "2020", "Ales"},
+				{"1.", "Film 2", "Actiune", "2020", "Ales"},
+				{"1.", "Film 2", "Actiune", "2020", "Ales"},
+				{"1.", "Film 2", "Actiune", "2020", "Ales"},
+				{"1.", "Film 2", "Actiune", "2020", "Ales"},
+				{"9.", "Film 10", "Actiune", "2020", "Ales"}
+		};
+		JTable tabel = new JTable(data, header);
+		JLabel lSort = new JLabel("Sortati dupa: ");
+
+		JRadioButton rbGen, rbAnProd;
+		rbGen = new JRadioButton("Gen: ");
+		rbAnProd = new JRadioButton("An productie: ");
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rbGen);
+		bg.add(rbAnProd);
+		JPanel pButSortare = new JPanel(new GridLayout(2, 2));
+		pButSortare.setPreferredSize(new Dimension(100, 100));
+		JComboBox<CategorieFilm> cbGen = new JComboBox<>(CategorieFilm.values());
+		pButSortare.add(rbGen);
+		pButSortare.add(cbGen);
+		pButSortare.add(rbAnProd);
+		pButSortare.add(new JTextField(4));
+
+		JScrollPane sp = new JScrollPane(tabel);
+		sp.setPreferredSize(new Dimension(300, 150));
+
+		JPanel pButoane = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JButton bRenuntati = new JButton("Inchideti");
+		pButoane.add(bRenuntati);
+		pButoane.add(new JButton("Inchiriati"));
+
+		getContentPane().removeAll();
+		getContentPane().add(lText, BorderLayout.NORTH);
+		getContentPane().add(sp, BorderLayout.WEST);
+		getContentPane().add(pButSortare, BorderLayout.EAST);
+		getContentPane().add(pButoane, BorderLayout.SOUTH);
+
+		validate();
+		pack();
+	}
+
+//	private void initPanelLogIn() {
+//		MaskFormatter formatter;
+//		JFormattedTextField serieCI = null;
+//		JLabel labelSerieCI;
+//		try {
+//			formatter = new MaskFormatter("??");
+//			labelSerieCI = new JLabel("Seria CI:");
+//			labelSerieCI.setLabelFor(serieCI);
+//			serieCI = new JFormattedTextField(formatter);
+//			serieCI.setText("");
+//			panelIntraCont.add(labelSerieCI);
+//			panelIntraCont.add(serieCI);
+//		} catch (ParseException e) {
+//			System.out.println(e.getMessage());
+//		}
+//
+//	}
+
+
+	class AscultatoriButoane implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == bExit) {
+				System.exit(0);
+			}
+			if (e.getSource() == bIntraCont) {
+				initPanelAlegereFilme();
+
+			}
 		}
+
 	}
 
 }
--- src/InchireriFilme.java
+++ src/InchireriFilme.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+import java.io.IOException;
+
 public class InchireriFilme {
 	public static void main(String[] args) {
-		new Fereastra();
+//		ListaFilme listaFilme = null;
+//		try {
+//			listaFilme = new ListaFilme();
+//		} catch (IOException e) {
+//			JOptionPane.showMessageDialog(null, "Aplicatia nu a putut fi lansata, lipseste lista filmelor.\nVa rugam " +
+//			                                    "sa contactati administratorul.", "Eroare initializare program",
+//			                              JOptionPane.ERROR_MESSAGE);
+//			System.exit(1);
+//		}
+
+		Fereastra f = new Fereastra("Inchiriere filme");
 	}
 
 }
