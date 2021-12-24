b714183b0f44d958d2ceeabddc437d9912e845d3
sergiu tica <mykymoto24@gmail.com>
2021-12-24 23:56:36 +0200

    Adaugati getteri

--- src/Film.java
+++ src/Film.java
@@ -7,7 +7,6 @@ public class Film {
 	private String numeFilm;
 	private CategorieFilm categorieFilm;
 
-
 	public Film(int nrCopii, int anProductie, String numeFilm, CategorieFilm categorieFilm) {
 		this.nrCopii = nrCopii;
 		this.anProductie = anProductie;
@@ -15,9 +14,32 @@ public class Film {
 		this.categorieFilm = categorieFilm;
 	}
 
+	public int getAnProductie() {
+		return anProductie;
+	}
+
+	public String getNumeFilm() {
+		return this.numeFilm;
+	}
+
+	public int getNrCopii() {
+		return this.nrCopii;
+	}
+
+	public CategorieFilm getCategorieFilm() {
+		return this.categorieFilm;
+	}
+
+	/**
+	 * @return Tipul de film, <code>Dvd</code> sau <code>Caseta</code>
+	 */
+	public String getTipFilm() {
+		return this.getClass().getName().substring(4);
+	}
+
 	@Override
 	public String toString() {
-		return numeFilm + " " + anProductie + " " + categorieFilm + " " + this.getClass().getName();
+		return numeFilm + " " + anProductie + " " + categorieFilm + " " + this.getClass().getName().substring(4);
 	}
 
 }
